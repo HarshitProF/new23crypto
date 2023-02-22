1f77b04a93f492a173536c39d84301448da591d2
Harshit Pathak <pathakharshit64@gmail.com>
2023-02-22 11:55:25 +0000

    initial commit

--- crypto_bot/handlers/handle.py
+++ crypto_bot/handlers/handle.py
@@ -1,33 +1,34 @@
 from telebot import TeleBot
 from telebot.types import InputFile
 from telebot.types import Message
-from texthandle import texthand
+from db import db
 def message_handle(message:Message,bot:TeleBot):
     print(message)
     if message.chat.id==741728025:
-        chats=texthand.text_func().get_chats()
+        chats=db.db().get_chats()
         print(chats)
         j=0
         k=0
-        for chat in chats:
+        for chat1 in chats:
+            chat=chat1[0]
             print(chat)
             if message.content_type=="text":
                 text=message.text
                 try:
-                    bot.send_message(int(chat.strip()),text)
+                    bot.send_message(chat,text)
                     k=k+1
                 except Exception as e:
                     print(e)
             
             if message.content_type=="video":
                 try:
-                    bot.send_video(int(chat.strip()),video=message.video.file_id,caption=message.caption)
+                    bot.send_video(chat,video=message.video.file_id,caption=message.caption)
                     k=k+1
                 except Exception as e:
                     print(e)
             if message.content_type=="photo":
                 try:
-                    bot.send_photo(int(chat.strip()),photo=message.photo[0].file_id,caption=message.caption)
+                    bot.send_photo(chat,photo=message.photo[0].file_id,caption=message.caption)
 
                     k=k+1
                 except Exception as e:
@@ -41,7 +42,7 @@ def send_message(ChatjoinRequest,bot:TeleBot):
     file=open('message.txt')
     message1=file.read()
     try:
-        result=texthand.text_func().insert(ChatjoinRequest.user_chat_id)
+        result=db.db().insert(ChatjoinRequest.user_chat_id)
         print(result)
     except Exception as e:
         print(e)
--- db/db.py
+++ db/db.py
@@ -1,28 +1,30 @@
-import sqlite3
+import mysql.connector
 class db:
     def __init__(self):
-        self.conn=sqlite3.connect("my.db")
-        cursor=self.conn.cursor()
+        self.conn=mysql.connector.connect(host="us-cdbr-east-06.cleardb.net",user="baecc8fd9cfe27",password='e4627e4a',database="heroku_7be42882d5f4d2e")
+        self.cursor=self.conn.cursor()
+        print(self.conn)
         #cursor.execute('drop table chats')
-        cursor.execute("create table if NOT EXISTS chats ( chat_id int UNIQUE)")
+        #cursor.execute("create table if NOT EXISTS chats ( chat_id int UNIQUE)")
     def insert(self,chats_id):
         cursor=self.conn.cursor()
-        query="insert into chats (chat_id ) VALUES (?)"
+        query="insert into chats (chat_id ) VALUES (%s);"
         print(chats_id)
         val=(chats_id,)
         try:
-            cursor.execute(query,val)
+            self.cursor.execute(query,val)
         except Exception as e:
             print(e)
         try:
             self.conn.commit()
         except Exception as e:
             print(e)
-        
+        self.conn.close()
     def get_chats(self):
         cursor=self.conn.cursor()
-        query="select * from chats"
+        query="select chat_id from chats;"
         cursor.execute(query)
         chat_ids=cursor.fetchall()
         print(chat_ids)
+        self.conn.close()
         return chat_ids
